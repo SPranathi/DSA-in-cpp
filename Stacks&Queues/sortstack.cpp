743f4b90f84773cc4ed2b74028081cecef0b096a
SPranathi <60268173+SPranathi@users.noreply.github.com>
2021-08-10 11:20:08 +0530

    Update sortstack.cpp

--- Stacks&Queues/sortstack.cpp
+++ Stacks&Queues/sortstack.cpp
@@ -20,3 +20,46 @@ Sample Input
 Sample Output
 1 2 3 4 5
 */
+#include<iostream>
+#include<stack>
+using namespace std;
+void sortstack(stack<int> &s){
+    stack<int> t;
+    while(!s.empty()){
+        if(t.empty()){
+            t.push(s.top());
+            s.pop();
+            continue;
+        }    
+        if(!t.empty() && t.top()>=s.top()){
+            t.push(s.top());
+            s.pop();
+        }
+        else{
+            int temp=s.top();
+            s.pop();
+            while(!t.empty() && temp>t.top()){
+                s.push(t.top());
+                t.pop();
+            }
+            t.push(temp);
+        }
+    }
+    s=t;
+}
+int main(){
+    int size;
+    cin>>size;
+    int value;
+    stack<int> s;
+    for(int i=0;i<size;i++){
+        cin>>value;
+        s.push(value);
+    }
+    sortstack(s);
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    cout<<endl;
+}
