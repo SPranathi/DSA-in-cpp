3e315fddb3af6fbd0488a9df33b5e0142cb72f13
SPranathi <60268173+SPranathi@users.noreply.github.com>
2021-08-20 16:39:12 +0530

    Add files via upload

---
+++ Stacks&Queues/InfixtoPostfix.cpp
@@ -0,0 +1,15 @@
+/*
+Given an infix expression S in the form of a string. You need to convert and print that input expression in postfix form. After that, evaluate and print the result of given expression in next line.
+Input Format :
+Infix expression
+Output Format :
+Line 1 : Corresponding postfix expression
+Line 2 : Result of input expression after evaluation
+Constraints :
+1 <= Length of S <= 100
+Sample Input 1 :
+3+4
+Sample Output 1 :
+34+
+7
+*/ 
