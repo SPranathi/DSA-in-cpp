94b28b742d733396d7a6834479217a99dee58007
SPranathi <60268173+SPranathi@users.noreply.github.com>
2021-08-31 21:55:43 +0530

    Update Dequeue.cpp

--- Stacks&Queues/Dequeue.cpp
+++ Stacks&Queues/Dequeue.cpp
@@ -17,3 +17,128 @@ Returns the element which is at front of the queue. Return -1 if queue is empty.
 7. getRear -
 Returns the element which is at end of the queue. Return -1 if queue is empty.
  */ 
+#include<iostream>
+using namespace std;
+class Deque{
+    int *deq;
+    int front;
+    int rear;
+    int n;
+
+    public:
+
+    Deque(int size){
+        deq=new int[size];
+        front=-1;
+        rear=-1;
+        n=size;
+    }
+
+    void insertfront(int ele){
+        if(front==-1 && rear==-1){
+            front=0;
+            rear=0;
+            deq[front]=ele;
+            return;
+        }
+        if((front+1)%n==rear){
+            cout<<(-1)<<endl;
+            return ;
+        }
+        front=(front+1)%n;
+        deq[front]=ele;
+    }
+
+    void insertrear(int ele){
+        if(front==-1 && rear==-1){
+            front=0;
+            rear=0;
+            deq[rear]=ele;
+            return;
+        }
+        if(front==rear-1 || rear==0 && front==n-1){
+            cout<<(1)<<endl;
+            return;
+        }
+        if(rear==0)
+            rear=n-1;
+        else
+            rear--;
+        deq[rear]=ele;
+    }
+
+    void deletefront(){
+        if(front==-1 && rear==-1){
+            cout<<(-1)<<endl;
+            return;
+        }
+        if(rear==front){
+            rear=-1;
+            front=-1;
+            return;
+        }
+        if(front==0)
+            front=n-1;
+        else
+            front--;
+    }
+
+    void deleterear(){
+        if(front==-1 && rear==-1){
+            cout<<(-1)<<endl;
+            return;
+        }
+        if(rear==front){
+            rear=-1;
+            front=-1;
+            return;
+        }
+        rear=(rear+1)%n;
+    }
+
+    int getfront(){
+        if(front==-1 && rear==-1)
+            return -1;
+        return deq[front];
+    }
+
+    int getrear(){
+        if(front==-1 && rear==-1)
+            return -1;
+        return deq[rear];
+    }
+};
+
+int main(){
+    Deque dq(10);
+    int choice,element;
+    while(true){
+        cout<<"Enter your choice:"<<endl;
+        cout<<"1.InsertFront\n2.InsertRear\n3.DeleteFront\n4.DeleteRear\n5.Getfront\n6.Getrear"<<endl;
+        cin>>choice;
+        switch(choice){
+            case 1:
+                cin>>element;
+                dq.insertfront(element);
+                break;
+            case 2:
+                cin>>element;
+                dq.insertrear(element);
+                break;
+            case 3:
+                dq.deletefront();
+                break;
+            case 4:
+                dq.deleterear();
+                break;
+            case 5:
+                cout<<dq.getfront()<<endl;
+                break;
+            case 6:
+                cout<<dq.getrear()<<endl;
+                break;
+            default:
+                return 0;
+        }
+    }
+}
