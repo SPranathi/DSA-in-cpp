bbdac83c8cbc75903910221a18cc206e49d0c266
SPranathi <60268173+SPranathi@users.noreply.github.com>
2021-11-10 16:52:29 +0530

    Add files via upload

---
+++ Priority Queue/KLargestElements.cpp
@@ -0,0 +1,56 @@
+/*
+You are given with an integer k and an array of integers that contain numbers in random order. Write a program to find k largest numbers from given array. You need to save them in an array and return it.
+Time complexity should be O(nlogk) and space complexity should be not more than O(k).
+Order of elements in the output is not important.
+Input Format :
+Line 1 : Size of array (n)
+Line 2 : Array elements (separated by space)
+Line 3 : Integer k
+Output Format :
+k largest elements
+Sample Input 1 :
+13
+2 12 9 16 10 5 3 20 25 11 1 8 6 
+4
+Sample Output 1 :
+25
+20
+16
+12
+ */
+
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+vector<int> Ksmallest(int *v,int n,int k){
+    priority_queue<int,vector<int>,greater<int>> pq;
+    vector<int> ans;
+    for(int i=0;i<k;i++)
+        pq.push(v[i]);
+    for(int i=k;i<n;i++){
+        if(v[i]>pq.top()){
+            pq.pop();
+            pq.push(v[i]);
+        }
+    }
+    while(!pq.empty()){
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    return ans;
+}
+int main(){
+    int n,k,s;
+    cin>>n;
+    int *v=new int[n];
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    cin>>k;
+    vector<int> arr=Ksmallest(v,n,k);
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<endl;
+    }
+}  
+
