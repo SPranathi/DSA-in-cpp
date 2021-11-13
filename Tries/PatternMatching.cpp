d05219b8a23283e7d68198c4f9903917c39866e2
SPranathi <60268173+SPranathi@users.noreply.github.com>
2021-11-13 09:21:01 +0530

    Add files via upload

---
+++ Tries/PatternMatching.cpp
@@ -0,0 +1,127 @@
+/*
+Given a list of n words and a pattern p that we want to search. Check if the pattern p is present the given words or not.
+Return true or false.
+Input Format :
+Line 1 : Integer n
+Line 2 : n words (separated by space)
+Line 3 : Pattern p (a string)
+Output Format :
+true ot false
+Sample Input 1 :
+4
+abc def ghi cba
+de
+Sample Output 2 :
+true
+Sample Input 2 :
+4
+abc def ghi hg
+hi
+Sample Output 2 :
+true
+Sample Input 3 :
+4
+abc def ghi hg
+hif
+Sample Output 3 :
+false
+ */ 
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class TrieNode{
+    public:
+    char data;
+    TrieNode **children;
+    bool isTerminal;
+
+    TrieNode(char data){
+        this->data=data;
+        children=new TrieNode*[26];
+        for(int i=0;i<26;i++)
+            children[i]=NULL;
+        isTerminal=false;
+    }
+};
+
+class Trie{
+    TrieNode *root;
+    public:
+    int count;
+
+    Trie(){
+        this->count=0;
+        root=new TrieNode('\0');
+    }
+    void insertword(TrieNode *root,string word){
+        //Base case
+        if(word.size()==0){
+            root->isTerminal=true;
+            return;
+        }
+        //small calculation
+        int index=word[0]-'a';
+        TrieNode *child;
+        if(root->children[index]!=NULL){
+            child=root->children[index];
+        }
+        else{
+            child=new TrieNode(word[0]);
+            root->children[index]=child;
+        }
+
+        //Recursive call
+        insertword(child,word.substr(1));
+    }
+
+    //For user
+    void insertword(string word){
+        insertword(root,word);
+    }
+
+    bool search(TrieNode *root,string word){
+        //Base case
+        if(word.size()==0){
+            return true;
+        }
+        //small calculation
+        int index=word[0]-'a';
+        bool ans;
+        if(root->children[index]!=NULL){
+            ans=search(root->children[index],word.substr(1));
+        }
+        else{
+            ans=false;
+        }
+        
+        return ans;
+    }
+
+    bool PatternMatching(vector<string> arr,string p){
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr[i].size();j++)
+                insertword(arr[i].substr(j));
+        }
+        return search(root,p);
+    }
+    
+};
+int main(){
+    Trie t;
+    int n;
+    cin>>n;
+    vector<string> arr;
+    string p;
+    for(int i=0;i<n;i++){
+        string word;
+        cin>>word;
+        arr.push_back(word);
+    }
+    cin>>p;
+    if(t.PatternMatching(arr,p))
+        cout<<"true"<<endl;
+    else
+        cout<<"false"<<endl;
+}
