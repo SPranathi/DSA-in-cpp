7c52e2f3a5b74a5a846b3023cf09b9c197f41002
SPranathi <60268173+SPranathi@users.noreply.github.com>
2021-11-16 18:29:50 +0530

    Create Fibonacci_using_memorization.cpp

---
+++ DP-1/Fibonacci_using_memorization.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int fib(int n){
+    if(n<=1)
+        return n;
+    return fib(n-1)+fib(n-2);
+}
+
+int fib_better(int n,int *ans){
+    if(n<=1){
+        ans[n]=n;
+        return n;
+    }
+    if(ans[n-1]==0)
+        ans[n-1]=fib_better(n-1,ans);
+    if(ans[n-2]==0)
+        ans[n-2]=fib_better(n-2,ans);
+    ans[n]=ans[n-1]+ans[n-2];
+    return ans[n];
+}
+int fib_better(int n){
+    int *ans=new int[n+1];
+    for(int i=0;i<=n;i++)
+        ans[i]=0;
+    return fib_better(n,ans);
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<fib_better(n)<<endl;
+}
